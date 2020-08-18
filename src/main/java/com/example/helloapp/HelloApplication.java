3aeefe6e77c698d81e33f67cf31a7e79788e8dd1
rishisinghal <singhal.rishi@gmail.com>
2020-08-18 15:46:49 +0530

    Update test file

--- src/main/java/com/example/helloapp/HelloApplication.java
+++ src/main/java/com/example/helloapp/HelloApplication.java
@@ -1,9 +1,7 @@
 package com.example.helloapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class HelloApplication {
--- src/main/java/com/example/helloapp/HelloController.java
+++ src/main/java/com/example/helloapp/HelloController.java
@@ -3,16 +3,14 @@ package com.example.helloapp;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.HashMap;
-
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Sample CI/CD in Google Cloud. " +
+        return "Sample CI/CD pipeline in Google Cloud. " +
                 "<br><br><center><big>Spring Boot + Github + Docker + Cloud Build + GCR + GKE" +
-                "<br><br> Push Code to Github -> Trigger Build -> Maven Test -> Maven Build -> " +
+                "<br><br> Push Code to Github branch -> Automatic Trigger Build -> Maven Test -> Maven Build -> " +
                 "Create Docker image -> Push image to GCR -> Deploy image to GKE";
     }
 }
