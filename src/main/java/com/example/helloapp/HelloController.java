a6c335d255e001b2f24d4c94771c25f6696d3b35
rishisinghal <singhal.rishi@gmail.com>
2020-08-18 16:13:12 +0530

    Update Message

--- src/main/java/com/example/helloapp/HelloController.java
+++ src/main/java/com/example/helloapp/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Sample CI/CD pipeline in Google Cloud. " +
+        return " Sample CI/CD pipeline in Google Cloud Platform. " +
                 "<br><br><center><big>Spring Boot + Github + Docker + Cloud Build + GCR + GKE" +
                 "<br><br> Push Code to Github branch -> Automatic Trigger Build -> Maven Test -> Maven Build -> " +
                 "Create Docker image -> Push image to GCR -> Deploy image to GKE";
