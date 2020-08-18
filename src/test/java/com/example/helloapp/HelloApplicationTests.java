b4eb649d0dd4e4d64137162d9d8a341378c71fc8
rishisinghal <singhal.rishi@gmail.com>
2020-08-18 15:36:05 +0530

    Update test file

--- src/test/java/com/example/helloapp/HelloApplicationTests.java
+++ src/test/java/com/example/helloapp/HelloApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringApplicationTests {
+class HelloApplicationTests {
 
 	@Test
 	void contextLoads() {
